d16202a70187b66a75c036061f956c0e149f04a5
zhongsifen <zhongsifen@gmail.com>
2017-12-13 22:19:47 +0800

    osea

---
+++ appXimTu/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  appXimTu
+//
+//  Created by SIFEN ZHONG on 3/12/2017.
+//  Copyright © 2017 ___ZHONGSIFEN___. All rights reserved.
+//
+
+#include <iostream>
+
+extern "C" {
+void easytest();
+void bxbep();
+}
+
+int main(int argc, const char * argv[]) {
+	easytest();
+	
+	std::cout << "Hello, World!\n";
+	return 0;
+}
